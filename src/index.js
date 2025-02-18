ebb1ba49e78abf589a967b239660a35f383a0b48
Theodor Kaljo <theodor.kaljo@gmail.com>
2025-02-18 19:42:22 +0200

    Add conversion to Fahrenheit and back to Celsius

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { format, parseISO } from 'date-fns';
 
 console.log('Hi');
 
@@ -6,6 +7,7 @@ const locationInput = document.querySelector("#location-input");
 const searchBtn = document.querySelector("#search-btn");
 const forecastDiv = document.querySelector("#forecast-div");
 const cityHeading = document.querySelector("#city-heading");
+const celsiusFahrenheitBtn = document.querySelector("#celsius-fahrenheit-btn")
 
 async function getWeather() {
     try {
@@ -15,6 +17,7 @@ async function getWeather() {
         if (!result.ok) {
             if (result.status === 400) {
                 clearPreviousLocation();
+                celsiusFahrenheitBtn.className = "hidden";
                 forecastDiv.textContent = `No such place as "${location}" was found in the database. Please check your input.`;
                 throw new Error("Location not found. Please check your input.");
             } else {
@@ -33,8 +36,11 @@ async function getWeather() {
 
 searchBtn.addEventListener("click", getWeather)
     
+
 function renderWeatherData(data) {
     clearPreviousLocation();
+
+    celsiusFahrenheitBtn.className = "";
 
     const weatherIcons = {
             "clear-day": "☀️",
@@ -60,13 +66,42 @@ function renderWeatherData(data) {
     const daysArray = data.days;
 
     for (let i = 0; i < 5; i++) {
-        console.log(`Day: ${daysArray[i].datetime}, weather: ${daysArray[i].conditions}`);
+        const unformattedDate = daysArray[i].datetime;
+        const formattedDate = format(parseISO(unformattedDate), "MMM dd (eee)");
+        console.log(`Day: ${formattedDate}, weather: ${daysArray[i].conditions}`);
         const dayDiv = document.createElement("div");
         dayDiv.className = "day-div";
 
-        const dateP = document.createElement("p");
-        dateP.textContent = daysArray[i].datetime;
-        dayDiv.appendChild(dateP);
+        const dateH3 = document.createElement("h3");
+        if (i === 0) {
+            dateH3.textContent = `Today, ${formattedDate}`
+        } else if (i === 1) {
+            dateH3.textContent = `Tomorrow, ${formattedDate}`
+        } else {
+            dateH3.textContent = formattedDate;
+        }
+        dayDiv.appendChild(dateH3);
+
+        const tempP = document.createElement("p");
+        let minTemp = convertToCelsius(daysArray[i].tempmin).toFixed(1);
+        let maxTemp = convertToCelsius(daysArray[i].tempmax).toFixed(1);
+        tempP.textContent = `${minTemp} ... ${maxTemp} degrees °C`;
+        dayDiv.appendChild(tempP);
+
+        function toggleCelsFahr() {
+            if (toggleCelsFahr.textContent === "Show ºF") {
+                minTemp = daysArray[i].tempmin;
+                maxTemp = daysArray[i].tempmax;
+                tempP.textContent = `${minTemp} ... ${maxTemp} degrees °F`;
+                toggleCelsFahr.textContent = "Show °C";
+            } else {
+                minTemp = convertToCelsius(daysArray[i].tempmin).toFixed(1);
+                maxTemp = convertToCelsius(daysArray[i].tempmax).toFixed(1);
+                tempP.textContent = `${minTemp} ... ${maxTemp} degrees °C`;
+                toggleCelsFahr.textContent = "Show ºF";
+            }
+        }
+        celsiusFahrenheitBtn.addEventListener("click", toggleCelsFahr);
 
         const iconFromAPI = daysArray[i].icon;
         const emojiDiv = document.createElement("div");
@@ -76,11 +111,12 @@ function renderWeatherData(data) {
         If it does not exist, it returns undefined, which is a falsy value. If the condition is true (key exists in weatherIcons). 
         The emoji corresponding to iconFromAPI is assigned to textContent. If the condition is false (key does not exist in weatherIcons). 
         The fallback value "❓" (question mark emoji) is used instead.*/
+        const weatherP = document.createElement("span");
+        weatherP.textContent = ` ${daysArray[i].conditions}`;
+        emojiDiv.appendChild(weatherP);
         dayDiv.appendChild(emojiDiv);
 
-        const weatherP = document.createElement("p");
-        weatherP.textContent = daysArray[i].conditions;
-        dayDiv.appendChild(weatherP);
+        
 
         forecastDiv.appendChild(dayDiv);
     }
@@ -89,4 +125,12 @@ function renderWeatherData(data) {
 function clearPreviousLocation() {
     forecastDiv.textContent = "";
     cityHeading.textContent = "";
+}
+
+function convertToCelsius(fahrenheit) {
+    return ((fahrenheit - 32) *5 ) / 9;
+}
+
+function convertToFahrenheit(celsius) {
+    return (celsius * 9) / 5 +32;   
 }
